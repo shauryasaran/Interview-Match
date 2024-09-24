648354ef06e8e13e217e11012dc1813103d5b3ec
rohannbajpai <rohanibajpai@gmail.com>
2024-09-24 19:44:56 -0400

    added app dir

--- video-interview-app/next.config.mjs
+++ video-interview-app/next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      appDir: true,
+    },
+  };
+  
+  export default nextConfig;
+  
